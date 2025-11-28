f0cdbedd06e2c30dcc885b96121650e38875ce76
cglremrcn <cglremrcn@gmail.com>
2025-11-28 12:36:33 +0300

    updated the sample.py for new conditioned model

--- sample.py
+++ sample.py
@@ -16,7 +16,7 @@ alphas_cumprod_prev = torch.cat([torch.tensor([1.0]).to(device), alphas_cumprod[
 sqrt_one_minus_alphas_cumprod = torch.sqrt(1.0 - alphas_cumprod).to(device)
 posterior_variance = betas * (1.0 - alphas_cumprod_prev) / (1.0 - alphas_cumprod)
 
-def p_sample(model,x,t,t_index):
+def p_sample(model,x,t,t_index,label):
 
 
     beta_t = betas[t_index]
@@ -25,8 +25,8 @@ def p_sample(model,x,t,t_index):
     
 
     t_tensor = torch.full((x.shape[0],), t_index, device=device, dtype=torch.long)
-
-    model_mean = sqrt_recip_alpha_t * (x - beta_t * model(x,t_tensor) / sqrt_one_minus_alpha_cumprod_t)
+    label_tensor = torch.full((x.shape[0],), label, device=device, dtype=torch.long)
+    model_mean = sqrt_recip_alpha_t * (x - beta_t * model(x,t_tensor,label_tensor) / sqrt_one_minus_alpha_cumprod_t)
 
 
     if t_index == 0:
@@ -38,12 +38,12 @@ def p_sample(model,x,t,t_index):
 
 
 
-def sample(model,shape):
+def sample(model,shape,target_label):
 
     img = torch.randn(shape,device=device)
 
     for i in reversed(range(0,TIME_STEPS)):
-        img = p_sample(model,img,i,i)
+        img = p_sample(model,img,i,i,target_label)
 
         if i % 20 == 0:
             print(f"Step {i}")
@@ -57,26 +57,37 @@ if __name__ == "__main__":
     
     model = MLPDiffusion().to(device)
     try:
-        model.load_state_dict(torch.load("diffusion_model.pth"))
+        model.load_state_dict(torch.load("diffusion_model_conditioned.pth"))
         print("Trained model loaded!")
     except:
-        print("Error: 'diffusion_model.pth' not found. Run train.py first!")
+        print("Error: 'diffusion_model_conditioned.pth' not found. Run train.py first!")
         exit()
         
     model.eval()
 
     
     with torch.no_grad():
-        generated_data = sample(model, shape=(1, 1000, 2))
+        data_disk = sample(model,shape=(1,1000,2),target_label=0)
+        data_ring = sample(model,shape=(1,1000,2),target_label=1)
 
     
-    data = generated_data[0].cpu().numpy() 
+    disk_np = data_disk[0].cpu().numpy()
+    ring_np = data_ring[0].cpu().numpy()
     
-    plt.figure(figsize=(6,6))
-    plt.scatter(data[:,0], data[:,1], s=5, c='purple', alpha=0.6)
-    plt.title("Generated Shape")
+    plt.figure(figsize=(10,7))
+    
+
+    plt.subplot(1,2,1)
+    plt.scatter(disk_np[:,0], disk_np[:,1], s=5, c='red', alpha=0.6)
+    plt.title("Generated Disk")
+    plt.axis('equal')
+    plt.grid(True, alpha=0.3)
+
+    plt.subplot(1,2,2)
+    plt.scatter(ring_np[:,0], ring_np[:,1], s=5, c='blue', alpha=0.6)
+    plt.title("Generated Ring")
     plt.axis('equal')
     plt.grid(True, alpha=0.3)
     plt.show()
-    
+
     print("Drawing completed!")
