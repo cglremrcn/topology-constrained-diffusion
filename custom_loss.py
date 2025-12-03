013a8db236df36f873d73df1c46c85d5fe1374cf
cglremrcn <cglremrcn@gmail.com>
2025-12-03 13:15:44 +0300

    changing the custom_loss.py for faster computation.

--- custom_loss.py
+++ custom_loss.py
@@ -1,49 +1,30 @@
 import torch
 import torch.nn as nn
-from torch_topological.nn import VietorisRipsComplex
 
 class TopologicalLoss(nn.Module):
     def __init__(self):
         super().__init__()
-        self.vr = VietorisRipsComplex(dim=1)
+        
 
     def forward(self, x, labels):
-        batch_results = self.vr(x)
+        """
+        x: [Batch, N, 2] -> Dot cloud
+        labels: [Batch] -> 0 (Disk) or 1 (Ring)
+        """
+        
+        radii = torch.norm(x, dim=2) 
         
         loss = torch.tensor(0.0, device=x.device)
-        count = 0
+        
+        
+        mask_ring = (labels == 1)
+        if mask_ring.any():
+            r_ring = radii[mask_ring]
+            loss += torch.mean((r_ring - 1.0) ** 2)
+            
+        mask_disk = (labels == 0)
+        if mask_disk.any():
+            r_disk = radii[mask_disk]
+            loss += torch.mean(torch.relu(r_disk - 1.0) ** 2)
 
-        for i, dim_list in enumerate(batch_results):
-            diagram = None
-            
-            for info in dim_list:
-                if info.dimension == 1:
-                    diagram = info.diagram
-                    break
-            
-            if diagram is None or len(diagram) == 0:
-                continue
-            
-            if not isinstance(diagram, torch.Tensor):
-                diagram = torch.tensor(diagram, device=x.device, dtype=torch.float32)
-
-            if len(diagram) == 0: continue
-
-           
-            lifetimes = diagram[:, 1] - diagram[:, 0]
-            
-            target = labels[i]
-            
-            if target == 1: 
-                if len(lifetimes) > 0:
-                    loss -= torch.max(lifetimes) 
-                    count += 1
-            
-            else: 
-                loss += torch.sum(lifetimes ** 2)
-                count += 1
-                
-        if count > 0:
-            return loss / count
-        else:
-            return loss
+        return loss
